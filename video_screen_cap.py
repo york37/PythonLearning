62efcfd10806346d3d280b19b06ab333c3e3b930
york37 <yue.gong@gmail.com>
2017-06-03 22:06:09 -0400

    add

--- video_screen_cap.py
+++ video_screen_cap.py
@@ -1,64 +1,82 @@
 import cv2
 import os
+import sys
+from PIL import Image
 
 # define video directory
+d = "D:/Work Related/A4"
 o = "C:/Users/yuego/Downloads/JAV_"
-d = "C:/Users/yuego/Downloads/JAV"
 
 file_list = os.listdir(d)
-print(file_list)
 
 for file_name in file_list:
     video_file = os.path.join(d, file_name)
-    output_file = os.path.join(o, file_name)
+    output_file = os.path.join(o, os.path.splitext(file_name)[0] + '.jpg')
+    print(file_name)
 
-    # capture from a video file
+    # capture video file properties
     cap = cv2.VideoCapture(video_file)
     length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     width  = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fps    = round(cap.get(cv2.CAP_PROP_FPS), 0)
     fourcc = str(cap.get(cv2.CAP_PROP_FOURCC))
-    print("Video length: ", length)
-    print("Video frame width: ", width)
-    print("Video frame length: ", height)
-    print("Video FPS: ", fps)
-    print("FourCC: ", fourcc)
+    # print("Video length: ", length)
+    # print("Video frame width: ", width)
+    # print("Video frame length: ", height)
+    # print("Video FPS: ", fps)
+    # print("FourCC: ", fourcc)
 
-    # Define the codec and create VideoWriter object
+    # Define the codec and set processing parameters
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter(output_file ,fourcc, fps, (int(width*0.25),int(height*0.25)), True)
+    segment_count = 60
 
-    counter = 0
-    cap_counter = 0
-    segment_count = 10
-
-    # calculate frame position at every 1/10 of video length
+    # calculate frame position by dividing total length by segment count
     positions = []
     p = int(length / segment_count)
     for i in range(segment_count):
         positions.append(p * i)
 
-    print(positions)
-
+    # read video frame at specific location and save the frame to a list
+    frame_list = []
     for i in range(segment_count):
         cap.set(cv2.CAP_PROP_POS_FRAMES, positions[i])
-        counter = 0
         while(cap.isOpened()):
             ret, frame = cap.read()
-            if counter > fps:
-                break
-            elif ret == True:
-                counter += 1
+            if ret == True:
                 frame = cv2.resize(frame, (0,0), fx=0.25, fy=0.25)
-                out.write(frame)
-
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    break
+                frame_list.append(frame)
+                sys.stdout.write("==>   %d  \r" % (i) )
+                sys.stdout.flush()
+                break
             else:
                 break
 
     # Release everything if job is finished
     cap.release()
-    out.release()
     cv2.destroyAllWindows()
+
+    # merge frames into a single image
+    horizonal_length = 5
+    vertical_length = int(segment_count / horizonal_length)
+    im = Image.fromarray(frame_list[0])
+    frame_width = im.width
+    frame_height = im.height
+    width = frame_width * horizonal_length
+    height = frame_height * vertical_length
+    new_image = Image.new('RGB', (width, height))
+
+    frame_index = 0
+    y_offset = 0
+    for i in range(vertical_length):
+        x_offset = 0
+        for j in range(horizonal_length):
+            im = frame_list[frame_index]
+            pil_im = Image.fromarray(cv2.cvtColor(im, cv2.COLOR_BGR2RGB))
+            new_image.paste(pil_im, (x_offset, y_offset))
+            x_offset += frame_width
+            frame_index += 1
+        y_offset += frame_height
+
+    # save the final image
+    new_image.save(output_file)
